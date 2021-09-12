cfb8f68977da5f52faf919463cd6dc2bdc08fc98
mac <dmitri-shadri@yandex.ru>
2021-09-12 21:54:56 +0300

    first commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+
+
+//    + get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/ViewsInfo.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class ViewsInfo {
+    private int count;
+
+    //    + get/set на все поля
+
+    public static class Geo {
+        private String type;
+        private String coordinates;
+
+    //    + get/set на все поля
+    }
+}
